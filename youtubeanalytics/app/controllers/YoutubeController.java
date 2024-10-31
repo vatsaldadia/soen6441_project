a2b2f04226bb73e60909b5a72db514c6b1d510f3
mirchandani-mohnish <mirchandani.mohnish03@gmail.com>
2024-10-31 16:13:47 -0400

    Init: Controller for youtube search

---
+++ youtubeanalytics/app/controllers/YoutubeController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.concurrent.CompletionStage;
+import javax.inject.Inject;
+import play.libs.ws.*;
+import play.mvc.*;
+
+public class YoutubeController extends Controller {
+
+    private final WSClient ws;
+    private static final String YOUTUBE_KEY =
+        "AIzaSyBn3hOC9y7PsDrQ62Xuj5M_P83ASq6GZRY";
+    private static final String YOUTUBE_URL =
+        "https://www.googleapis.com/youtube/v3/search";
+
+    public YoutubeController(WSClient ws) {
+        this.ws = ws;
+    }
+
+    public CompleteionStage<Result> searchVideos(String query) {
+        return ws
+            .url(YOUTUBE_URL)
+            .addQueryParameter("part", "snippet")
+            .addQueryParameter("maxResults", "10")
+            .addQueryParameter("q", query)
+            .addQueryParameter("type", "video")
+            .addQueryParameter("key", YOUTUBE_API_KEY)
+            .get()
+            .thenApply(response -> {
+                if (response.getStatus() == 200) {
+                    return ok(response.asJson());
+                } else {
+                    return internalServerError(
+                        "YouTube API error: " + response.getBody()
+                    );
+                }
+            });
+    }
+}
